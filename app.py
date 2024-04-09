4534064c8a09f37ec29532574e8d4a690a9e6335
amal_jith <amaljith5828@gmail.com>
2024-04-09 11:52:01 +0530

    first

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+app.debug=True
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/myWorks")
+def myWorks():
+    return render_template("myWorks.html")
+
+@app.route("/myResume")
+def myResume():
+    return render_template("myResume.html")
+
+app.run()
